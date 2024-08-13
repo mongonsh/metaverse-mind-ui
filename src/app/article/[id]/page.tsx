7d3ed939a8331c608aa8604bd3618536c74e73e1
mungunshagaitumurb <mungunshagai.tumurb@gmail.com>
2024-08-13 12:26:22 +0900

    add

--- src/app/article/[id]/page.tsx
+++ src/app/article/[id]/page.tsx
@@ -2,6 +2,7 @@ import { axiosInstance } from "@/utils/csrf";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { useState, useEffect } from "react";
+import Spinner from "@/components/Spinner";
 export async function generateStaticParams() {
   const backendUrl = `https://metaverse-mind.vercel.app`;
 
@@ -26,6 +27,13 @@ export default async function Article({ params }: any) {
   return (
     <>
       <Header />
+      {!data && (
+        <div className="h-[500px] block ">
+          <div className="  absolute top-[60px] bg-white-950 right-0 left-0 bottom-0 inset-0 flex justify-center items-center ">
+            <Spinner />
+          </div>
+        </div>
+      )}
       <main>
         <div className="container mx-auto py-4">
           <div className="bg-white shadow-md rounded-lg p-6">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,10 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Card from "@/components/Card";
 import SpecialCard from "@/components/SpecialCard";
+import Spinner from "@/components/Spinner";
 
 const Home = () => {
-  let [articles, setArticles] = useState([]);
+  let [articles, setArticles] = useState(null);
   const backendUrl = "https://metaverse-mind.vercel.app";
 
   let getAllArticles = async () => {
@@ -24,23 +25,31 @@ const Home = () => {
   return (
     <>
       <Header />
-      <main className="container mx-auto py-8 px-4">
-        <div className="h-[500px] block">
-          <div className=" bg-gray-950 absolute top-[60px] right-0 left-0 bottom-0 inset-0  ">
-            <img src="/cover.png" className="mx-3" />
-
-            <section className="mb-8">
-              <SpecialCard data={articles[0]} />
-            </section>
+      {!articles && (
+        <div className="h-[500px] block ">
+          <div className="  absolute top-[60px] bg-gray-950 right-0 left-0 bottom-0 inset-0 flex justify-center items-center ">
+            <Spinner />
           </div>
         </div>
+      )}
+      {articles && (
+        <main className="container mx-auto py-8 px-4">
+          <div className="h-[500px] block ">
+            <div className="  absolute top-[60px] bg-gray-950 right-0 left-0 bottom-0 inset-0  ">
+              <img src="/cover.png" className="mx-3" />
+              <section className="mb-8">
+                <SpecialCard data={articles[0]} />
+              </section>
+            </div>
+          </div>
 
-        <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {articles.map((article, idx) => {
-            return <Card key={idx} data={article} />;
-          })}
-        </section>
-      </main>
+          <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {articles.map((article, idx) => {
+              return <Card key={idx} data={article} />;
+            })}
+          </section>
+        </main>
+      )}
       <Footer />
     </>
   );
